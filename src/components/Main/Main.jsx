5374d9744e94fedaa627bfebd7e6ade21eaf5387
Afasarya <coderea9@gmail.com>
2024-06-27 06:41:23 +0700

    final change

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,55 +1,132 @@
-import React from 'react'
-import './Main.css'
-import { assets } from '../../assets/assets'
-
-
+import { useContext } from "react";
+import { assets } from "../../assets/assets";
+import "./Main.css";
+import { Context } from "../../context/Context";
 const Main = () => {
-  return (
-    <div className='main'>
-      <div className="nav">
-        <p>Rea</p>
-        <img src={assets.user_icon} alt="" />
-      </div>
-      <div className="main-container">
-        <div className="greet">
-            <p><span>Hello, Arya.</span></p>
-            <p>How can I help you today?</p>
-        </div>
-        <div className="cards">
-            <div className="card">
-                <p>Suggest beautiful places to see on a upcoming road trip</p>
-                <img src={assets.compass_icon} alt="" />
-            </div>
-            <div className="card">
-                <p>Briefly summarize this concept: urban planning</p>
-                <img src={assets.bulb_icon} alt="" />
-            </div>
-            <div className="card">
-                <p>Brainstorm team bonding activities for our work retreat</p>
-                <img src={assets.message_icon} alt="" />
-            </div>
-            <div className="card">
-                <p>Suggest beautiful places to see on a upcoming road trip</p>
-                <img src={assets.code_icon} alt="" />
-            </div>
-        </div>
+	const {
+		onSent,
+		recentPrompt,
+		showResults,
+		loading,
+		resultData,
+		setInput,
+		input,
+	} = useContext(Context);
 
-        <div className="main-bottom">
-            <div className="search-box">
-                <input type="text" placeholder='Enter a prompt here' />
-                <div>
-                    <img src={assets.gallery_icon} alt="" />
-                    <img src={assets.mic_icon} alt="" />
-                    <img src={assets.send_icon} alt="" />
-                </div>
-            </div>
-            <p className='bottom-info'>
-                Rea-AI Powered by Gemini may display inacurrate info, including about people, so double-check its responses. Your Privacy and Rea Apps
-            </p>
-        </div>
-      </div>
-    </div>
-  )
-}
+    const handleCardClick = (promptText) => {
+			setInput(promptText);
+		};
+	return (
+		<div className="main">
+			<div className="nav">
+				<p>Rea-AI</p>
+				<img src={assets.user} alt="" />
+			</div>
+			<div className="main-container">
+				{!showResults ? (
+					<>
+						<div className="greet">
+							<p>
+								<span>Hello , Dev </span>
+							</p>
+							<p>How Can i Help You Today?</p>
+						</div>
+						<div className="cards">
+							<div
+								className="card"
+								onClick={() =>
+									handleCardClick("Suggest Some Place To Visit In Kerala")
+								}
+							>
+								<p>Suggest Some Place To Visit In Kerala </p>
+								<img src={assets.compass_icon} alt="" />
+							</div>
+							<div
+								className="card"
+								onClick={() =>
+									handleCardClick(
+										"Brainstorm team bonding activities for our work retreat"
+									)
+								}
+							>
+								<p>Brainstorm team bonding activities for our work retreat </p>
+								<img src={assets.message_icon} alt="" />
+							</div>
+							<div
+								className="card"
+								onClick={() =>
+									handleCardClick("How to Create a Gyroscope using Disc?")
+								}
+							>
+								<p>How to Create a Gyroscope using Disc?</p>
+								<img src={assets.bulb_icon} alt="" />
+							</div>
+							<div
+								className="card"
+								onClick={() => {
+									handleCardClick(
+										"Create a Script for the youtube video about coding "
+									);
+								}}
+							>
+								<p>Create a Script for the youtube video about coding </p>
+								<img src={assets.code_icon} alt="" />
+							</div>
+						</div>
+					</>
+				) : (
+					<div className="result">
+						<div className="result-title">
+							<img src={assets.user} alt="" />
+							<p>{recentPrompt}</p>
+						</div>
+						<div className="result-data">
+							<img src={assets.gemini_icon} alt="" />
+							{loading ? (
+								<div className="loader">
+									<hr />
+									<hr />
+									<hr />
+								</div>
+							) : (
+								<p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+							)}
+						</div>
+					</div>
+				)}
 
-export default Main
+				<div className="main-bottom">
+					<div className="search-box">
+						<input
+							onChange={(e) => {
+								setInput(e.target.value);
+							}}
+							value={input}
+							type="text"
+							placeholder="Enter the Prompt Here"
+						/>
+						<div>
+							<img src={assets.gallery_icon} alt="" />
+							<img src={assets.mic_icon} alt="" />
+							<img
+								src={assets.send_icon}
+								alt=""
+								onClick={() => {
+									onSent();
+								}}
+							/>
+						</div>
+					</div>
+					<div className="bottom-info">
+						<p>
+							Gemini may display inaccurate info, including about people, so
+							double-check its responses. Your privacy & Gemini Apps
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Main;
